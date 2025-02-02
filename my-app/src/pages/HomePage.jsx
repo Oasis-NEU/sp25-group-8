c6cf229c75d1a698138bd581d8f07b43c7033bc0
amoghathimamula <amoghathimamula@gmail.com>
2025-02-02 13:18:02 -0500

    added new intro message

--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -1,1 +1,15 @@
-import React from 'react'
+import { useState } from 'react'
+import './HomePage.css'
+
+function HomePage() {
+    return (
+        <>
+            <h1> Welcome to NUDailyPlanner! </h1>
+            <p className="info">
+                Our goal is to help first-years plan their days in college!
+            </p>
+        </>
+    )
+}
+// this is a test
+export default HomePage
