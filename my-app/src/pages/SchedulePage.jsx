c1b09259651d0e75278caa05bb1d1ae7d76cef56
amoghathimamula <amoghathimamula@gmail.com>
2025-02-16 13:21:39 -0500

    added link button in schedulespage to pref page

--- my-app/src/pages/SchedulePage.jsx
+++ my-app/src/pages/SchedulePage.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import ContainedButtons from '../components/HomeButton.jsx'
+import { useNavigate } from 'react-router-dom';
 
 function SchedulePage() {
+    const nav = useNavigate();
     return (
         <>
             <h1> Welcome to NUDailyPlanner! </h1>
+            <ContainedButtons onClick={() => nav('/preferences')} />
         </>
+
     )
 }
 // this is a test
