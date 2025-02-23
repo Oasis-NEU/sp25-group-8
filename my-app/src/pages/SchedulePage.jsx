e41511f1c73768bc8929a84d93209736925ec788
amoghathimamula <amoghathimamula@gmail.com>
2025-02-23 13:10:00 -0500

    fixed a buncha shit like routing and stuff

--- my-app/src/pages/SchedulePage.jsx
+++ my-app/src/pages/SchedulePage.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import ContainedButtons from '../components/ContainedButtons.jsx'
-import './SchedulePage.css'
-import AddButton from '../components/addButton.jsx'
-import ClassInfo from '../components/classInfo.jsx'
+import { useState } from 'react';
+import ContainedButtons from '../components/ContainedButtons.jsx';
+import './SchedulePage.css';
+import AddButton from '../components/addButton.jsx';
+import ClassInfo from '../components/classInfo.jsx';
 import { useNavigate } from 'react-router-dom';
 
 function SchedulePage() {
     const [showClassInfo, setShowClassInfo] = useState(false);
     const nav = useNavigate();
+
     return (
         <>
-            <h1> Input your schedule here! </h1>
-            <AddButton message='Click here to add a Class' 
-            onClick={() => setShowClassInfo(!showClassInfo)}/>
+            <h1>Input your schedule here!</h1>
+            <AddButton
+                message='Click here to add a Class'
+                onClick={() => setShowClassInfo(!showClassInfo)}
+            />
             {showClassInfo && <ClassInfo />}
-            <h1> Input your schedule here! </h1>
-            <addButton onClick={() => classInfo}/>
-            <ContainedButtons onClick={() => nav('/preferences')} />
+            <div style={{ margin: '100px 0' }}></div>
+            <ContainedButtons
+                message='Choose your preferences!'
+                onClick={() => nav('/preferences')} />
         </>
-
-    )
+    );
 }
-export default SchedulePage
+
+export default SchedulePage;
