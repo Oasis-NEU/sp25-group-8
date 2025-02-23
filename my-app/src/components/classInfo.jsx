b58675b8b23e848f64b22726f8574cdbe49b7392
Aditya <appathak46@gmail.com>
2025-02-23 13:26:23 -0500

    style

--- my-app/src/components/classInfo.jsx
+++ my-app/src/components/classInfo.jsx
@@ -30,17 +30,17 @@ export default function ClassInfo(){
           <label>
             Where is your class?
             <select value={location} onChange={(e) => setLocation(e.target.value)}>
-                <option value = "East Village">EastVillage</option>
-                <option value = "EXP">EXP</option>
-                <option value = "ISEC">ISEC</option>
-                <option value = "Richards">Richards</option>
-                <option value = "Ell Hall">EllHall</option>
-                <option value = "Churchill">Churchill</option>
-                <option value = "West Village">WestVillage</option>
-                <option value = "Robinson">Robinson</option>
-                <option value = "Mugar Life Sciences">Mugar</option>
-                <option value = "International Village">International</option>
-                <option value = "Behrakis">Behrakis</option>
+            <option value="East Village">East Village</option>
+            <option value="EXP">EXP</option>
+            <option value="ISEC">ISEC</option>
+            <option value="Richards">Richards</option>
+            <option value="Ell Hall">Ell Hall</option>
+            <option value="Churchill">Churchill</option>
+            <option value="West Village">West Village</option>
+            <option value="Robinson">Robinson</option>
+            <option value="Mugar Life Sciences">Mugar Life Sciences</option>
+            <option value="International Village">International Village</option>
+            <option value="Behrakis">Behrakis</option>
             </select>
           </label>
         </div>
