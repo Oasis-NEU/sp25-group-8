ba8682144b8a30bd0c0eeff756572f088ec32705
amoghathimamula <amoghathimamula@gmail.com>
2025-03-07 13:24:56 -0500

    added user input for preferences

--- my-app/src/pages/PreferencesPage.jsx
+++ my-app/src/pages/PreferencesPage.jsx
@@ -1,18 +1,85 @@
-import { useState } from 'react'
-import './PreferencesPage.css'
+import { useState } from 'react';
+import './PreferencesPage.css';
 import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Card, CardContent, IconButton, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
-function PrefencesPage() {
+const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+// have to change logic once database is confirmed to send inputted data and make schedule
+function PreferencesPage() {
     const nav = useNavigate();
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        setTasks([...tasks, { name: "", occurrence: [], duration: "" }]);
+    };
+
+    const updateTask = (index, field, value) => {
+        const updatedTasks = tasks.map((task, i) =>
+            i === index ? { ...task, [field]: value } : task
+        );
+        setTasks(updatedTasks);
+    };
+
+    const removeTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
+
     return (
         <>
             <h1> Along with study time, eating time, and relaxation time, please list any other activities you would like to do! </h1>
-            <p className="info">
-                Our goal is to help first-years plan their days in college!
-            </p>
+
+            {tasks.map((task, index) => (
+                <Card key={index} sx={{ marginBottom: 5, padding: 2 }}>
+                    <CardContent>
+                        <TextField
+                            label="Task Name"
+                            value={task.name}
+                            onChange={(e) => updateTask(index, "name", e.target.value)}
+                            fullWidth
+                            margin="normal"
+                        />
+
+                        {/* Multi-Select Dropdown for Days */}
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel>Days</InputLabel>
+                            <Select
+                                multiple
+                                value={task.occurrence}
+                                onChange={(e) => updateTask(index, "occurrence", e.target.value)}
+                                renderValue={(selected) => selected.join(", ")}
+                            >
+                                {daysOfWeek.map((day) => (
+                                    <MenuItem key={day} value={day}>
+                                        {day}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        {/* Numeric Input for Duration */}
+                        <TextField
+                            label="Duration (hours)"
+                            type="number"
+                            value={task.duration}
+                            onChange={(e) => updateTask(index, "duration", e.target.value)}
+                            fullWidth
+                            margin="normal"
+                        />
+
+                        <IconButton onClick={() => removeTask(index)}>
+                            <Delete />
+                        </IconButton>
+                    </CardContent>
+                </Card>
+            ))}
+
+            <Button variant="contained" onClick={addTask} sx={{ marginTop: 2 }}>
+                Add Task
+            </Button>
         </>
-    )
+    );
 }
-// this is a test
-export default PrefencesPage
+
+export default PreferencesPage;
